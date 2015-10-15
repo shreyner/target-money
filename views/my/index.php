41464f8dfeed500a9ab522fda0139a79330e1329
Alexander Shreyner <alexander@shreyner.ru>
2015-10-15 20:08:58 +0400

    feature statusbar

--- views/my/index.php
+++ views/my/index.php
@@ -14,9 +14,16 @@
   <tbody>
     <?php foreach ($model as $key => $value): ?>
       <tr>
-        <td><?= $value->name ?></td>
-        <td><?= $value->itog ?>+<?= $value->target ?></td>
-        <td>Создать</td>
+        <td class="col-md-3"><?= $value->name ?></td>
+        <!-- <td><?= $value->itog ?>+<?= $value->target ?></td> -->
+        <td>
+          <div class="progress progress-striped">
+            <div class="progress-bar progress-bar-success" role="progressbar" style="width: <?php echo $value->itog /( $value->target / 100) ?>%;color:black;">
+              <?= $value->itog ?>/<?= $value->target ?>
+            </div>
+          </div>
+        </td>
+        <td class="col-md-1">Добавить</td>
       </tr>
     <?php endforeach; ?>
   </tbody>
