18de64a2f770db4b8c5aaf42923050644b086dff
Alexander Shreyner <alexander@shreyner.ru>
2015-10-16 13:51:33 +0400

    fixed historyAchiev

--- models/HistoryAchiev.php
+++ models/HistoryAchiev.php
@@ -33,7 +33,7 @@ class HistoryAchiev extends \yii\db\ActiveRecord
         return [
             [['money', 'id_target'], 'required'],
             [['money', 'id_target'], 'integer'],
-            [['money'],'number','min'=>1,'max'=>2147483646]
+            [['money'],'number','min'=>1,'max'=>2147483646],
             [['description'], 'string', 'max' => 50]
         ];
     }
